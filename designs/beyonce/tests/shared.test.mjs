7cc1c5ee9efb1b08dae685e46d8e81adfa95b78c
Jamie Morgenstern <jamiemorgenstern@Jamies-MacBook-Pro-4.local>
2022-11-14 21:13:55 -0800

    beyonce bunka

---
+++ designs/beyonce/tests/shared.test.mjs
@@ -0,0 +1,19 @@
+// This file is auto-generated | Any changes you make will be overwritten.
+
+//
+
+import { Beyonce } from '../src/index.mjs'
+
+// Shared tests
+import { testPatternConfig } from '../../../tests/designs/config.mjs'
+import { testPatternDrafting } from '../../../tests/designs/drafting.mjs'
+import { testPatternSampling } from '../../../tests/designs/sampling.mjs'
+
+// Test config
+testPatternConfig(Beyonce)
+
+// Test drafting - Change the second parameter to `true` to log errors
+testPatternDrafting(Beyonce, false)
+
+// Test sampling - Change the second parameter to `true` to log errors
+testPatternSampling(Beyonce, false)
